fcc495fc8d0e84597d2c32532ab59c3cc474af07
zsj0613 <zsj06130675@163.com>
2015-12-26 17:58:13 +0800

    项目成型

--- ConstructionSafety/Page_Construction_Safety.xaml.cs
+++ ConstructionSafety/Page_Construction_Safety.xaml.cs
@@ -22,6 +22,8 @@ namespace ConstructionSafety
         public Page_Construction_Safety()
         {
             InitializeComponent();
+
+            SetLinearGradientUnderline();
         }
 
         private void button_Construction_Safety_BackHome_Click(object sender, RoutedEventArgs e)
@@ -39,6 +41,40 @@ namespace ConstructionSafety
             {
                 pageFrame.Source = new Uri("Page_Main.xaml",UriKind.RelativeOrAbsolute);
             }
+        }
+
+
+        private void textBlock_openDoc_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MainWindow.setTextColorBlue(sender);
+        }
+
+        private void textBlock_openDoc_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MainWindow.setTextColorBlack(sender);
+        }
+
+        private void textBlock_openDoc_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"ResFiles\灌河大桥工程项目安全生产管理用表（2012年8月8日）.doc");
+        }
+
+        private void SetLinearGradientUnderline()
+        {
+            TextDecoration myUnderline = new TextDecoration();
+
+            Pen myPen = new Pen();
+            myPen.Brush = new LinearGradientBrush(Colors.Yellow, Colors.Red, new Point(0, 0.5), new Point(1, 0.5));
+            myPen.Brush.Opacity = 0.8;
+            myPen.Thickness = 5;
+            myPen.DashStyle = DashStyles.Dash;
+            myUnderline.Pen = myPen;
+            myUnderline.PenThicknessUnit = TextDecorationUnit.FontRecommended;
+            myUnderline.PenOffset = 10; //设置下划线距离文字的偏移
+
+            TextDecorationCollection myCollection = new TextDecorationCollection();
+            myCollection.Add(myUnderline);
+            textBlock_openDoc.TextDecorations = myCollection;
         }
     }
 }
